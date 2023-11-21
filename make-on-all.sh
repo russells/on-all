fe149167b71e5251984e721a530c272b508f28f7
Russell Steicke <russells@adelie.cx>
2023-11-21 12:24:35 +0800

    t/20231121-on-all.sh (5/5 commits) 20231121-122435.470363539 wa-aifs-ws77

--- make-on-all.sh
+++ make-on-all.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+SRC="$1"
+PROG="$2"
+
 if [ -x /bin/bash ] ; then
 	THIS_IS_BASH="/bin/bash"
 else
 	THIS_IS_BASH="/usr/bin/env bash"
 fi
-
-SRC="$1"
-PROG="$2"
 
 set -x
 sed < "$SRC" > "$PROG" -e '1s|/bin/bash|'"$THIS_IS_BASH"'|'
